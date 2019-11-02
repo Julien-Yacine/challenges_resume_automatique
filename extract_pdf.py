e425014fd62f75c5fb527d3a039c312d1952ebea
Thomas <thomas.nicolas11@gmail.com>
2019-11-02 11:33:06 +0100

    Modifi class extract pdf pour remise à 0 du texte à chaque iteration

--- extract_pdf.py
+++ extract_pdf.py
@@ -14,18 +14,19 @@ class extract_pdf:
     def __init__(self, path):
         self.path = path
         self.allfiles = [f for f in listdir(self.path) if isfile(join(self.path, f))]
-        print(self.allfiles)
+        # print(self.allfiles)
 
     def convert_pdf_to_txt(self):
-        rsrcmgr = PDFResourceManager()
-        retstr = StringIO()
-        codec = 'utf-8'
-        laparams = LAParams()
-        device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
-        pdf_folder = Path(self.path)
-        print(self.allfiles)
 
+        pdf_folder = Path(self.path)
+        # print(self.allfiles)
+        list_txt = list()
         for files in self.allfiles:
+            rsrcmgr = PDFResourceManager()
+            retstr = StringIO()
+            codec = 'utf-8'
+            laparams = LAParams()
+            device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
             print(files)
             fp = open(pdf_folder / files, 'rb')
             interpreter = PDFPageInterpreter(rsrcmgr, device)
@@ -33,19 +34,24 @@ class extract_pdf:
             maxpages = 0
             caching = True
             pagenos = set()
-            list_txt = list()
 
             for page in PDFPage.get_pages(fp, pagenos, maxpages=maxpages, password=password, caching=caching,
                                           check_extractable=True):
                 interpreter.process_page(page)
 
             text = retstr.getvalue()
+            print("Longueur du doc : ")
+            print(len(text))
+            print("Début du doc : ")
+            print(text[1:100])
             list_txt.append(text)
+            print(len(list_txt))
             fp.close()
+            device.close()
+            retstr.close()
 
         return list_txt
-        device.close()
-        retstr.close()
+
 
 toto = extract_pdf('./echantillons_pdf')
 toto2 = toto.convert_pdf_to_txt()
