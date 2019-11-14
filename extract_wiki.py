8d5acbb771f54b3b3497b0029ec7afe7db576ab4
Thomas <thomas.nicolas11@gmail.com>
2019-11-14 22:25:28 +0100

    biz

--- extract_wiki.py
+++ extract_wiki.py
@@ -7,7 +7,7 @@ import re
 
 
 class extract_portal_wikipedia:
-
+    
     def __init__(self, url_portal):
         self.url_portal = url_portal
 
@@ -25,24 +25,50 @@ class extract_portal_wikipedia:
         for i in tmp_links:
             article = re.findall('/wiki/(.*?)$', i)
             if article != []:
-                urls.append(article)
+                urls.append(unquote(article[0]))
         return urls
 
     def extract_resumes(self, urls):
         resumes = []
-        articles = []
+        articles = [] 
+        titres = [] 
+        wiki_wiki = wikipediaapi.Wikipedia(language='fr', extract_format=wikipediaapi.ExtractFormat.WIKI) 
         for url in urls:
-            print(f'Récupération résumés de {url}')
-            try:
-                resume = wikipedia.summary(url)
-                if resume != '':
-                    resumes.append(resume)
-                    article = wikipedia.WikipediaPage(url).content
+            print(f'je test {url}')
+            resume = wiki_wiki.page(url)
+            if resume.exists():
+                print(f'{url} existe')
+                resumes.append(resume.summary)
+                if resume.text!='' and resume.text != None:
+                    article = resume.text
+                    #article = re.search("\\n\\n", article).start()
+                    article = re.sub('(\\n\\n).*(\\n)', "", article)
+                    article = re.sub("\\n", "", article)
                     articles.append(article)
-            except Exception:
-                print(f'Je suis INCAPABLE DE RECUPERER {url}')
-                pass
-        return resumes, articles
+                else:
+                    articles.append(resume.text) 
+                titres.append(resume.title)
+            else:
+                print(f"{url} n'existe pas, on essaie donc {url.replace('_', ' ')}")
+                resume = wiki_wiki.page(url.replace('_', ' '))
+
+                if not resume.exists():
+                    print(f"{url}  & {url.replace('_', ' ')} n'existe pas")
+                    resume = None
+                else:
+                    resumes.append(resume.summary)
+                    if resume.text!='':
+                        article = re.sub('(\\n\\n).*(\\n)', "", article)
+                        article = re.sub("\\n", "", article)
+                        articles.append(article)
+                    else:
+                        articles.append(resume.text) 
+                    titres.append(resume.title)
+            del(url)
+
+        return  titres, resumes, articles
+
+
 
 portail_economie = extract_portal_wikipedia('https://fr.wikipedia.org/w/index.php?title=Cat%C3%A9gorie:Portail:%C3%89conomie/Articles_li%C3%A9s&from=B')
 url_economie = portail_economie.extract_url()
