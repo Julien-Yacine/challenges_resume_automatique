09451d828f840ae26d46e72612258236b14cb4c2
Thomas <thomas.nicolas11@gmail.com>
2019-11-16 16:24:43 +0100

    Boucle extracion sur toutes les pages

--- extract_wiki.py
+++ extract_wiki.py
@@ -11,10 +11,33 @@ class extract_portal_wikipedia:
     
     def __init__(self, url_portal):
         self.url_portal = url_portal
+        
+    def scrap_all_url_from_portal(self):
+        super_links = []
+        regexp = re.compile(r'page suivante')
+        url = self.url_portal
+        for k in range(0,2):
+            print(k)
+            req = requests.get(url)
+            soup = BeautifulSoup(req.text, "lxml")
+            for i in soup.find_all(name='a', href=True):
+                if regexp.search(str(i)):
+                    #print(re.findall('href="(.*?)#', str(i)))
+                    tmp_super_link = re.findall('href="(.*?)#', str(i))
 
-    def extract_url(self):
+                    tmp_super_link = re.sub('amp;', "", str(tmp_super_link))
+                    super_links.append('https://fr.wikipedia.org/'+str(tmp_super_link)[2:-2])
+                    break
+            url = super_links[k]
+            print(url)
 
-        req = requests.get(self.url_portal)
+        return super_links
+        
+    
+
+    def extract_url(self,super_link):
+
+        req = requests.get(super_link)
         soup = BeautifulSoup(req.text, "lxml")
 
         tmp_links = []
@@ -70,21 +93,25 @@ class extract_portal_wikipedia:
         return  titres, resumes, articles
 
 
-portail_economie = extract_portal_wikipedia('https://fr.wikipedia.org/w/index.php?title=Cat%C3%A9gorie:Portail:%C3%89conomie/Articles_li%C3%A9s&from=B')
-url_economie = portail_economie.extract_url()
-resumes = portail_economie.extract_resumes(url_economie[0:20])
 
-import pandas as pnd
-df = pnd.DataFrame(resumes)
-
-df_transpose = df.T
-df_transpose.columns = ['Titre', 'Resume', 'Texte']
-df_transpose['summary_lenght']=df_transpose['Resume'].apply(len)
-
-# Creation du text sans le resume
-text=[]
-for i in enumerate(df_transpose['summary_lenght']):
-    text.append(df_transpose.iloc[i[0],2][i[1]:])
-
-# Ajout au dataframe panda
-df_transpose['text']=text
+portail_economie = extract_portal_wikipedia('https://fr.wikipedia.org/w/index.php?title=Cat%C3%A9gorie:Portail:%C3%89conomie/Articles_li%C3%A9s')
+print('Extraction des SUPERS URLS')
+super_links = portail_economie.scrap_all_url_from_portal()
+#print(super_links)
+super_df = pnd.DataFrame()
+print('Extraction du contenu des SUPERS URLS')
+for super_link in super_links:
+    print(super_link)
+    url_economie = portail_economie.extract_url(super_link)
+    resumes = portail_economie.extract_resumes(url_economie[4:])
+    df = pnd.DataFrame(resumes)
+    df_transpose = df.T
+    df_transpose.columns = ['Titre', 'Resume', 'Texte']
+    df_transpose['summary_lenght']=df_transpose['Resume'].apply(len)
+    text=[]
+    for i in enumerate(df_transpose['summary_lenght']):
+        #print(i)
+        #print(df_transpose.iloc[i[0],2][i[1]:])
+        text.append(df_transpose.iloc[i[0],2][i[1]:])
+    df_transpose['text']=text
+    super_df = super_df.append(df_transpose)
